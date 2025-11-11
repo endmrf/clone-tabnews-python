bd71fb451031d5b2d7ef0239cd7b2c19b38ef2f2
Endryo <endryofeitosa@gmail.com>
2025-11-10 22:01:09 -0300

    ajusta build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Criando e ativando ambiente virtual..."
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 echo "Coletando arquivos estáticos..."
 python3 clone_tabnews_python/manage.py collectstatic --noinput
