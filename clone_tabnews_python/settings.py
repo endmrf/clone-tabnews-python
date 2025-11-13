666ad825fb7fc7c43a672d45a093cda420a76305
Endryo <endryofeitosa@gmail.com>
2025-11-12 21:23:51 -0300

    configura o sys.path

--- clone_tabnews_python/settings.py
+++ clone_tabnews_python/settings.py
@@ -14,8 +14,6 @@ import os
 import sys
 from pathlib import Path
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- clone_tabnews_python/wsgi.py
+++ clone_tabnews_python/wsgi.py
@@ -11,6 +11,7 @@ import os
 import traceback
 import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clone_tabnews_python.settings')
 
 try:
